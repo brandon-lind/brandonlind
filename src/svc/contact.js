c6609c491ae069b7315253e3928c6a3f384a0bb3
Brandon Lind <linddeveloper@gmail.com>
2018-11-22 22:24:10 +0000

    Fixups for the email sender

--- src/svc/contact.js
+++ src/svc/contact.js
@@ -1,12 +1,47 @@
 import { success, failure } from './libs/response';
+import AWS from 'aws-sdk';
 
-export default async function main(event) {
-  const data = JSON.parse(event.body);
+AWS.config.update({region: 'us-east-1'});
 
+export async function main(event) {
   try {
-    return success({ status: true, data });
+    console.log('Starting contact email send...');
+    
+    const data = JSON.parse(event.body);  
+    
+    await sendEmail(data);
+    
+    return success({ message:`Email sent`, status:true });
   } catch (e) {
     console.log(e);
-    return failure({ status: false });
+    return failure({ message:`Email failed to send`, status: false });
   }
+}
+
+async function sendEmail (event) {
+  const RECEIVER = process.env.EMAIL_SES_RECEIVER;
+  const SENDER = process.env.EMAIL_SES_SENDER;
+  const params = {
+      Destination: {
+          ToAddresses: [
+              RECEIVER
+          ]
+      },
+      Message: {
+          Body: {
+              Text: {
+                  Data: 'name: ' + event.name + '\nemail: ' + event.email + '\nmessage: ' + event.message,
+                  Charset: 'UTF-8'
+              }
+          },
+          Subject: {
+              Data: 'Website Referral Form: ' + event.name,
+              Charset: 'UTF-8'
+          }
+      },
+      Source: SENDER
+  };
+  
+  console.log(`Sending email: ${JSON.stringify(params)}`);
+  return new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
 }
